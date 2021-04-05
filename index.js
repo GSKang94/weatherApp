dd33a1639989ca25c356722def24c7c74e1dfefc
GSingh1994 <singh.gagandeep0034@gmail.com>
2021-04-04 22:52:27 -0500

    Code rephrased to respect factory functions

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
-let userInput = document.querySelector(".userInput");
-let submitBtn = document.querySelector(".submit");
-
-let input = "";
-
-userInput.onchange = (e) => (input = e.target.value);
-
-submitBtn.onclick = () => getWeather();
+const getUserInput = () => {
+  let userInput = document.querySelector(".userInput").value;
+  document.querySelector(".submit").onclick = () => getWeather();
+  return { userInput };
+};
+getUserInput();
 
 async function getWeather() {
+  const user = getUserInput();
   let apiKey = "fce51865df10c606b4200dd86db4fdd5";
-  let weatherApi = `api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}`;
-  //   let response = await fetch(weatherApi);
-  //   let data = await response.json();
-  //   console.log(data);
-  console.log(weatherApi);
-  //   fetch(weatherApi)
-  //     .then((response) => response.json())
-  //     .then((text) => console.log(text));
+  let weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${user.userInput}&appid=${apiKey}&units=metric`;
+  let response = await fetch(weatherApi);
+  let data = await response.json();
+  return displayWeather(data);
 }
 
-//   fetch(weatherApi).then((response) => console.log(response.json()));
-
-//   fetch(weatherApi)
-//     .then((response) => response.json())
-//     .then((data) => appendData(data));
-
-//   function appendData(data) {
-//     console.log(data);
-//   }
-//   appendData();
+function displayWeather(data) {
+  let display = document.querySelector("#display");
+  console.log(data);
+}
